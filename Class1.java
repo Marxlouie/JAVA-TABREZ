bec60e4905151738b5021a4b493823a5fd0973da
Marx louis J <109841183+Marxlouie@users.noreply.github.com>
2024-11-18 08:07:41 +0530

    Create Class1.java
    Answers to first class's assignment

---
+++ Class1.java
@@ -0,0 +1,118 @@
+class Bestf 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Brijo");
+		System.out.println("Jamal");
+		System.out.println("Sridhar");
+		System.out.println("Manny");
+	}
+}
+
+class Bestf x
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Brijo");
+		System.out.print("Jamal");
+		System.out.print("Sridhar");
+		System.out.print("Manny");
+	}
+}
+
+class Details 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("22");
+		System.out.print("6380387390");
+		System.out.print("Marx");
+		System.out.print("20.05.2002");
+	}
+}
+
+class Mobile 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("S21FE");
+		System.out.print("75000");
+		System.out.print("white");
+		System.out.print("8GB");
+	}
+}
+
+class laptop
+{
+    public static void main(Strings[] args)
+    {
+     System.out.println("Lenovo");
+     System.out.println("intel i3");
+     System.out.println("Windows10");
+     System.out.println("80,000");
+    }
+}
+
+class Bike
+{
+    public static void main(Strings[] args)
+    {
+     System.out.println("Continental gt");
+     System.out.println("1,50,000");
+     System.out.println("Blue");
+    }
+}
+
+class Car
+{
+    public static void main(Strings[] args)
+    {
+     System.out.println("Mini cooper S");
+     System.out.println("Red");
+     System.out.println("44,00,000");
+    }
+}
+
+class Abc
+ {
+
+
+ }
+
+class Abc
+ {
+    public static void main(Strings[] args)
+    {
+
+    
+     }
+ }
+
+class Display
+ {
+    public static void main(Strings[] args)
+    {
+     System.out.println("Syed Tabrez");
+     System.out.println("Java Faculty");
+         }
+ }
+
+class Display
+ {
+    public static void main(Strings[] args)
+    {
+     System.out.println(""Syed Tabrez"");
+     System.out.println(""Java Faculty"");
+         }
+ }
+
+
+
+
+
+
+
+
+
+
+
