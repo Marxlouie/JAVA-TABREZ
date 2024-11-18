1c7935cbc5465ece689b22457768ba62a216f271
Marx louis J <109841183+Marxlouie@users.noreply.github.com>
2024-11-18 19:13:01 +0530

    Create Java2.java

---
+++ Class programs/Java2.java
@@ -0,0 +1,40 @@
+class  Name
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("\"Marx louis\"");
+	}
+}
+
+class Java2
+{ 
+	public static void main(String[] args)
+	{
+		System.out.println("JAVA");
+		System.out.println("\"JA\"VA\"");
+		System.out.println("SQL");
+		System.out.println("\"S\"QL\"");
+
+	}
+}
+
+class Hotel
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\t\t\t\"Buhari\"");
+		System.out.println("A2B");
+		System.out.println("\t\t\t\"Bilal\"");
+		System.out.print("Amma \"Canteen\"");
+	}
+}
+
+class Demo4
+{
+  public static void main(String[]args){
+  
+  System.out.println("1.Java\n2.Sql\n3.html\n4.css");
+  
+  }
+}
+
