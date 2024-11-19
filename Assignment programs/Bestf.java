e0ac2c86856e37d195635e89f7915762ab665c3b
Marx louis J <109841183+Marxlouie@users.noreply.github.com>
2024-11-19 20:53:51 +0530

    Add files via upload

---
+++ Assignment programs/Bestf.java
@@ -0,0 +1,136 @@
+class  BestfName
+{
+	public static void main(String[] args) 
+	{   String a = "Jamal";
+	    String b = "Angel";
+		String c = "Sridhar";
+		String d = "Manny";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
+
+class  Contact
+{
+	public static void main(String[] args) 
+	{   int age = 22;
+	    long phone_number = 6380387393l;
+		
+		System.out.println(age);
+		System.out.println(phone_number);
+		
+	}
+}
+
+class  Marks
+{
+	public static void main(String[] args) 
+	{   String a = "94.6%";
+	    String b = "67.5%";
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
+
+class Grades
+{ 
+	public static void main(String[] args){
+	int Science = 99;
+	int Social  = 97;
+	int Maths   = 87;
+
+    System.out.println(Science);
+	System.out.println(Social);
+	System.out.println(Maths);
+	}
+}
+
+class Emp
+{ 
+	public static void main(String[] args){
+	int id = 01;
+	String Ename  = "Issac";
+	long Salary  = 80000L;
+	String Hiredate  = "01-JAN-2002";
+
+    System.out.println(id);
+	System.out.println(Ename);
+	System.out.println(Salary);
+	System.out.println(Hiredate);
+	}
+}
+
+class Student
+{ 
+	public static void main(String[] args){
+	int Sid = 101;
+	String Sname  = "Alwin";
+	long Mark  = 495L;
+	String Gender  = "Male";
+	long contact  = 9345610572L;
+
+    System.out.println(Sid);
+	System.out.println(Sname);
+	System.out.println(Mark);
+	System.out.println(Gender);
+	System.out.println(contact);
+	}
+}
+
+class Dreamv
+{ 
+	public static void main(String[] args){
+	String Dcar  = "MINI cooper S";
+	String Dbike = "Continental GT";
+
+    System.out.println(Dcar);
+	System.out.println(Dbike);
+	
+	}
+}
+
+class Futsal
+{ 
+	public static void main(String[] args){
+	long sal  = 3000000L;
+	
+
+    System.out.println(sal);
+	
+	
+	}
+}
+
+
+
+class Crush
+{ 
+	public static void main(String[] args){
+	String crush = "Jhanvi kapoor";
+
+    System.out.println(crush);
+	
+	
+	}
+}
+
+
+class Favsong
+{ 
+	public static void main(String[] args){
+	String song = "Aasaigal";
+	String singer = "Giftson Durai";
+	int age = 27;
+
+    System.out.println(song);
+	System.out.println(singer);
+	System.out.println(age);
+	
+	
+	}
+}
+
